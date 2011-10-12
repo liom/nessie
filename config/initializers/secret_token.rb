79c7be84fa62272b7b4e6c78f7d463410924aba2
Dmitry Shaposhnik <dmitry@shaposhnik.name>
2011-10-12 22:38:50 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nessie::Application.config.secret_token = '34685864cdcd25e5733016303b0015dcfd189e293aa8cf26f2617bea9b2c748d5008d8661a04ce7dfa49b7a4fc1c568c3d01a40238e6d988b670f7968eff9fb4'
