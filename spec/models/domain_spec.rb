01fc4bc24f4f4a510b052efd51648e8e0a637fdd
Dmitry Shaposhnik <dmitry@shaposhnik.name>
2011-10-13 23:45:58 +0300

    Fixing domain name uniqueness spec.

--- spec/models/domain_spec.rb
+++ spec/models/domain_spec.rb
@@ -1,10 +1,18 @@
 require 'spec_helper'
 
 describe Domain do
+
   # Domain name. Required.
   # Example: example.com
-  it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:name) }
+  describe "name" do
+    before do
+      FactoryGirl.create(:domain)
+    end
+    it { should validate_presence_of(:name) }
+    it "should be unique" do
+      should validate_uniqueness_of(:name)
+    end
+  end
   
   # Domain type. One of MASTER, SLAVE, NATIVE. Required.
   it { should validate_presence_of(:type) }
