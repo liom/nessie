46166c5d549e022cdb88619a5631491e8a1a6d9e
Dmitry Shaposhnik <dmitry@shaposhnik.name>
2011-10-13 22:26:38 +0300

    Creating corresponding fields for domain record. Fields are taken from powerdns model.

--- db/migrate/20111013191153_create_domains.rb
+++ db/migrate/20111013191153_create_domains.rb
@@ -2,7 +2,16 @@ class CreateDomains < ActiveRecord::Migration
   def change
     create_table :domains do |t|
 
+      t.string  :name,            :null => false
+      t.string  :master,          :null => true
+      t.integer :last_check,      :null => true,  :default => nil
+      t.string  :type,            :null => false
+      t.integer :notified_serial, :null => true, :default => nil
+      t.string  :account,         :null => true, :default => nil
+
       t.timestamps
     end
+    
+    add_index :domains, :name, :unique => true
   end
 end
