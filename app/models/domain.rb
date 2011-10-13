849b2b3fbd8879642c2689ba7d0366159c792b42
Dmitry Shaposhnik <dmitry@shaposhnik.name>
2011-10-13 23:31:11 +0300

    Adding validations to make test pass.

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,2 +1,6 @@
 class Domain < ActiveRecord::Base
+  ALLOWED_TYPES = %w(MASTER SLAVE NATIVE)
+  
+  validates :name, :presence => true, :uniqueness => true
+  validates :type, :presence => true, :inclusion => { :in => ALLOWED_TYPES }
 end
