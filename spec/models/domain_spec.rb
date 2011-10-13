ec1ec63b068d8ba0b8d757b952e692c38377d0f9
Dmitry Shaposhnik <dmitry@shaposhnik.name>
2011-10-13 23:15:04 +0300

    Adding basic tests for domains.

--- spec/models/domain_spec.rb
+++ spec/models/domain_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Domain do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Domain name. Required.
+  # Example: example.com
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name) }
+  
+  # Domain type. One of MASTER, SLAVE, NATIVE. Required.
+  it { should validate_presence_of(:type) }
+  it { should allow_value('MASTER').for(:type) }
+  it { should allow_value('SLAVE').for(:type) }
+  it { should allow_value('NATIVE').for(:type) }
+  it { should_not allow_value("test").for(:type) }
+
 end
