cc7c602f4e09ecd8a0ab127dba5942e2bf80e78f
Dmitry Shaposhnik <dmitry@shaposhnik.name>
2011-10-29 18:22:15 +0300

    Adding specs for A-record.

--- spec/models/record_a_spec.rb
+++ spec/models/record_a_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
 
 describe RecordA do
-  it { should validate_presence_of(:content) }
-  it "should not be able to change record type" do
-    a = FactoryGirl.build(:record_a)
-    a.type.should == 'A'
-    a.type = 'MX'
-    a.type.should == 'A'
+  before :each do
+    @record = FactoryGirl.build(:record_a)
   end
+  
+  it { should validate_presence_of(:content) }
+  
+  it "should not be able to change record type" do
+    @record.type.should == 'A'
+    @record.type = 'MX'
+    @record.type.should == 'A'
+  end
+  
+  it "should allow valid IP address" do
+    @record.content = '1.2.3.4'
+    @record.should be_valid
+  end
+  
+  it "should disallow invalid IP address" do
+    @record.content = '1.2.3.256'
+    @record.should_not be_valid
+  end
+  
 end
