52f110727fcb3aa1dafaf77d139a7daff5c45d96
Dmitry Shaposhnik <dmitry@shaposhnik.name>
2012-06-13 14:02:12 +0300

    Matcher simplified.

--- spec/support/matchers/immutable_attribute.rb
+++ spec/support/matchers/immutable_attribute.rb
@@ -1,9 +1,6 @@
 RSpec::Matchers.define :have_immutable_field do |attribute, value|
   match do |model|
-    # expect { model.send("#{attribute}=", Object.new) }.not_to change{model.send(attribute)}
-
-    model.send(attribute).should == value
-    model.attributes[attribute] = Object.new
-    model.send(attribute).should == value
+    model.send("#{attribute}=", Object.new)
+    model.send(attribute) == value
   end
 end
